3ef5628436238a2569dd650a4a3becd12da5c402
Oleg Agafonov <oleg.agafonov@center2m.ru>
2023-03-07 18:44:49 +0300

    feat: реализовал функцию для вычисления факториала числа

--- palindrome-number/main.py
+++ palindrome-number/main.py
@@ -1,2 +1,10 @@
-###
-print("hi")
+# n != 1 * 2 * 3 * .. * n-1 * n
+def factorial(n: int) -> int:
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+if __name__ == "__main__":
+    print(factorial(10))
